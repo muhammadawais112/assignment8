929e6e732cf3c7de23073f56c203a008c60af40a
muhammadawais112 <simpleboy1917@gmail.com>
2022-01-01 22:37:10 +0500

    assignment8

---
+++ ass8.js
@@ -0,0 +1,95 @@
+
+
+document.write("assignment 8");
+
+
+
+// program :4// console.log("MONTH CHECKING",mon[mons]);
+// var monCheck1 = new Date("10/11/2009");
+
+// console.log(monCheck1);
+// var mon = monCheck1.getMonth();
+// // var monCheck2=new Date("10/11/2009");
+// // console.log(monCheck2);
+// var monCheck2 = new Date("11/11/2009");
+// console.log(monCheck2);
+// var mon2 = monCheck2.getMonth();
+// var monthname = ["jan", "feb", "march", "april", "may", "june", "july", "august", "sep", "october", "november", "december"];
+// console.log(monthname[mon]);
+// console.log(monthname[mon2]);
+
+
+
+// checking equal program 5
+// var Date1 = new Date("11/14/2013 00:00");
+// var Date2 = new Date("11/14/2013 00:01");
+// if(Date1 == Date2) {
+//     console.log("Date1==Date2", Date1 == Date2);
+
+// if (Date1 > Date2) {
+//     console.log("Date1>Date2", Date1 > Date2);
+// }
+// else {
+//     console.log("Date1<Date2 ", Date1 < Date2);
+// }
+
+// } 
+
+
+
+// program 9
+// var date =new Date();
+// console.log(date);
+// var lastDayM=date.getMonth();
+
+// console.log("enter a month",lastDayM);
+// var totalDay =["31","29","31","30","31","30","31","31","30","31","30","31"];
+// console.log(" last day in month",totalDay[lastDayM]);
+
+
+
+// program 10
+// var maxDate = ["2015/02/01", "2015/02/02", "2015/01/03"];
+// if (maxDate[0] > maxDate[1]) 
+//     {
+//     console.log("maximum date ", maxDate[0]);
+
+//     elseif(maxDate[0] > maxDate[2])
+//         console.log("maximum date ", maxDate[1]);
+//     }
+
+// else {
+//     console.log("maximum date ", maxDate[2]);
+// }
+
+
+
+// program 12
+// var minDate = ["2015/02/01", "2015/02/02", "2015/01/03"];
+// if (minDate[0] < minDate[1]) 
+//     {
+  
+//      if(minDate[0] < minDate[2]){
+//         console.log("manimum date ", minDate[0]);
+//      }
+//     else{
+//         console.log("manimum date ", minDate[2]);
+//     }
+//     }
+
+// else {
+//     console.log("manimum date ", minDate[2]);
+// }
+
+
+
+// program 8
+// var dates=new Date(days)
+// var days=[04/02/2014 ,11/04/2014,12/02/2014,11/04/2014];
+// var diff1=days[0]-days[1];
+// var diff2=days[2]-days[3];
+// console.log(diff1);
+// console.log(diff2);
+
+
+
